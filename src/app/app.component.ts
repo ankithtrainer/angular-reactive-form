405dea393d17639e6ec8f55b48e42ee231c6ddc5
ankithtrainer <ankith.trainer@gmail.com>
2024-10-19 21:06:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reactive-form';
+  myForm: FormGroup;
+
+  constructor() {
+    this.myForm = new FormGroup({
+      // Define your form controls here
+      firstName: new FormControl('Promod'),
+      lastName: new FormControl('Rai'),
+      email: new FormControl('promodrai@gmail.com')
+    });
+  }
+
+  onSubmit() {
+    // Handle form submission here
+    console.log(this.myForm.value);
+  }
 }
